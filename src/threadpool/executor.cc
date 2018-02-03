fc4f18ec493561c0cffd16096ff3733a1e011a61
Dominique <dominique.besson@epita.fr>
2018-02-03 19:27:18 +0100

    Executor: better handled run function and added status accesors

--- src/threadpool/executor.cc
+++ src/threadpool/executor.cc
@@ -1,8 +1,9 @@
 #include "executor.hh"
+#include <iostream>
 
-Executor::Executor(const std::shared_ptr<std::recursive_mutex>& mutex,
-                   const std::shared_ptr<std::condition_variable_any>& cv,
-                   std::queue<std::function<void()>>& queue)
+Executor::Executor(std::recursive_mutex& mutex,
+        std::condition_variable_any& cv,
+        std::queue<std::function<void()>>& queue)
     : cv_(cv), mutex_(mutex), queue_(queue)
 {
     state = State::INITED;
@@ -20,26 +21,49 @@ void Executor::run()
 {
     if (state == State::STOPPED)
         return;
-
-    state = State::RUNNING;
-    while (state == State::RUNNING)
+    
+    state = State::WAITING;
+    while (state == State::WAITING)
     {
-        std::unique_lock<std::recursive_mutex> lock(*mutex_);
-        while (!need_to_run()) cv_->wait(lock);
-        std::function<void ()> f = queue_.front();
-        queue_.pop();
-        lock.unlock();
+        mutex_.lock();
+        try{
+            while (!need_to_run() && state != State::STOPPED){
 
-        f();
+                std::cout << "is WAITING" << std::endl;
+                cv_.wait(mutex_);
+            }
+            if (state == State::STOPPED)
+            {
+                mutex_.unlock();
+                return;
+            }
+            state = State::RUNNING;
+            std::function<void ()> f = queue_.front();
+            queue_.pop();
+            mutex_.unlock();
+                
+            f();
+            state = State::WAITING;
+        }
+        catch(const std::exception&)
+        {
+            mutex_.unlock();
+        }
     }
 }
 
 bool Executor::need_to_run()
 {
-    return queue_.size() != 0;
+    return queue_.size();
 }
 
 void Executor::stop()
 {
     state = State::STOPPED;
+    my_thread_.detach();
+}
+
+bool Executor::is_waiting()
+{
+    return state == State::WAITING;
 }
