2d51a2e0a45112c64bee57cd6d1e52e385dd1943
Dominique <dominique.besson@epita.fr>
2018-02-03 09:17:50 +0100

    Executor: implemented constructor and start()

--- src/threadpool/executor.cc
+++ src/threadpool/executor.cc
@@ -1,13 +1,17 @@
 #include "executor.hh"
 
 Executor::Executor(const std::shared_ptr<std::recursive_mutex>& mutex,
-                   const std::shared_ptr<std::condition_variable>& cv)
-    : cv_(cv), mutex_(mutex)
-{
-    std::make_shared<std::thread>(start);
-}
+                   const std::shared_ptr<std::condition_variable>& cv,
+                   std::queue<std::function<void()>>& queue)
+    : cv_(cv), mutex_(mutex), queue_(queue)
+{}
 
 void Executor::start()
+{
+    my_thread_ = std::thread(&Executor::run, this);
+}
+
+void Executor::run()
 {
 
 }
