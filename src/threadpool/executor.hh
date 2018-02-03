0ebfdc72d5da9b2ad726fb9a74816c9fdb0838f6
Dominique <dominique.besson@epita.fr>
2018-02-03 09:48:08 +0100

    Executor: added state enum

--- src/threadpool/executor.hh
+++ src/threadpool/executor.hh
@@ -9,6 +9,12 @@
 
 class Executor
 {
+    enum class State
+    {
+        RUNNING,
+        STOPPED,
+        INITED
+    };
     public:
         Executor(const std::shared_ptr<std::recursive_mutex>& mutex,
                  const std::shared_ptr<std::condition_variable>& cv,
@@ -23,4 +29,6 @@ class Executor
         const std::shared_ptr<std::recursive_mutex> mutex_;
         std::queue<std::function<void ()>>& queue_;
         std::thread my_thread_;
+
+        enum State state;
 };
