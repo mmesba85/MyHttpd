98b9924c4cff587263851d1817ed8fb7cb513d40
Dominique <dominique.besson@epita.fr>
2018-02-03 01:12:43 +0100

    ThreadPool: implemented contructor

--- src/threadpool/thread-pool.cc
+++ src/threadpool/thread-pool.cc
@@ -1,3 +1,14 @@
-#include "threadpool.hh"
+#include "thread-pool.hh"
 
+ThreadPool::ThreadPool(unsigned nb_max_threads, bool start_now)
+{
+    if (nb_max_threads == 0)
+        throw std::invalid_argument(
+                "ThreadPool must be created with at least one thread");
 
+    for(unsigned i = 0; i < nb_max_threads; ++i)
+        threads_.push_back(std::make_shared<Executor>(cv_, mutex_));
+
+    if (start_now)
+        start();
+}
