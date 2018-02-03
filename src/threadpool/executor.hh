8811f800ad820d1c28dd240fc166d79609918ad4
Dominique <dominique.besson@epita.fr>
2018-02-03 01:13:22 +0100

    Executor: added methods and attributes

--- src/threadpool/executor.hh
+++ src/threadpool/executor.hh
@@ -1,12 +1,22 @@
 #pragma once
 
 #include <thread>
+#include <condition_variable>
+#include <memory>
+#include <mutex>
 
 class Executor
 {
     public:
+        Executor(const std::shared_ptr<std::recursive_mutex>& mutex,
+                 const std::shared_ptr<std::condition_variable>& cv);
         void start();
+        void stop();
     private:
-        std::thread my_thread_;
+        void run();
+        bool need_to_wait();
 
+        const std::shared_ptr<std::condition_variable> cv_;
+        const std::shared_ptr<std::recursive_mutex> mutex_;
+        std::shared_ptr<std::thread> my_thread_;
 };
