f9a58466034a403cd7603f250a251889c4e8412a
Dominique <dominique.besson@epita.fr>
2018-02-03 10:19:29 +0100

    Executor: changed condition_variable type and implemented run

--- src/threadpool/executor.cc
+++ src/threadpool/executor.cc
@@ -1,7 +1,7 @@
 #include "executor.hh"
 
 Executor::Executor(const std::shared_ptr<std::recursive_mutex>& mutex,
-                   const std::shared_ptr<std::condition_variable>& cv,
+                   const std::shared_ptr<std::condition_variable_any>& cv,
                    std::queue<std::function<void()>>& queue)
     : cv_(cv), mutex_(mutex), queue_(queue)
 {
@@ -24,8 +24,19 @@ void Executor::run()
     state = State::RUNNING;
     while (state == State::RUNNING)
     {
+        std::unique_lock<std::recursive_mutex> lock(*mutex_);
+        cv_->wait(lock, &Executor::need_to_run);
+        std::function<void ()> f = queue_.front();
+        queue_.pop();
+        lock.unlock();
 
+        f();
     }
+}
+
+bool Executor::need_to_run()
+{
+    return queue_.size() != 0;
 }
 
 void Executor::stop()
