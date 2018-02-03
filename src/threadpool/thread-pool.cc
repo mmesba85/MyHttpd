936bdc9610e68ca747f14fb2894c3675e258f51e
Dominique <dominique.besson@epita.fr>
2018-02-03 09:09:05 +0100

    ThreadPool: change tasks type and implemented start

--- src/threadpool/thread-pool.cc
+++ src/threadpool/thread-pool.cc
@@ -7,8 +7,17 @@ ThreadPool::ThreadPool(unsigned nb_max_threads, bool start_now)
                 "ThreadPool must be created with at least one thread");
 
     for(unsigned i = 0; i < nb_max_threads; ++i)
-        threads_.push_back(std::make_shared<Executor>(cv_, mutex_));
+        threads_.push_back(std::make_shared<Executor>(cv_, mutex_, tasks_));
 
+    state = State::INITED;
     if (start_now)
         start();
+}
+
+void ThreadPool::start()
+{
+    if (state == State::STARTED)
+        return;
+    for (auto& executor : threads_)
+        executor->start();
 }
