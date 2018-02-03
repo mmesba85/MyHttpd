459baa3cb23564f3d1773ab9e688032358c5e542
Dominique <dominique.besson@epita.fr>
2018-02-03 01:12:30 +0100

    ThreadPool: refatored functions and variables

--- src/threadpool/thread-pool.hh
+++ src/threadpool/thread-pool.hh
@@ -5,18 +5,20 @@
 #include <list>
 #include <vector>
 #include <functional>
+#include <memory>
+#include <exception>
 
 #include "executor.hh"
 
-#define DEFAULT_MAX_THREADS 10
+#define DEFAULT_MAX_THREADS 1
 
 class ThreadPool
 {
     public:
-        ThreadPool(unsigned nb_max_threads = DEFAULT_MAX_THREADS);
+        ThreadPool(unsigned nb_max_threads = DEFAULT_MAX_THREADS, bool start_now
+                = true);
         void add_task(std::function<void ()>);
         void remove_all_tasks();
-        void init(bool start = true);
         void destroy();
         void start();
 
@@ -24,5 +26,5 @@ class ThreadPool
         std::recursive_mutex mutex_;
         std::condition_variable cv_;
         std::list<std::function<void ()>> tasks_;
-        std::vector<Executor> threads_;
+        std::vector<std::shared_ptr<Executor>> threads_;
 };
