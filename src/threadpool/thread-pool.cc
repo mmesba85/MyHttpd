064c1d8c94472189187490f69d9a9ff01a68c903
Dominique <dominique.besson@epita.fr>
2018-02-03 19:26:15 +0100

    threadPool: added a sync method and corrected attrobutes errors

--- src/threadpool/thread-pool.cc
+++ src/threadpool/thread-pool.cc
@@ -1,5 +1,7 @@
 #include "thread-pool.hh"
 
+#include <iostream>
+
 ThreadPool::ThreadPool(unsigned nb_max_threads, bool start_now)
 {
     if (nb_max_threads == 0)
@@ -23,20 +25,20 @@ void ThreadPool::start()
         executor->start();
 }
 
-void ThreadPool::add_task(std::function<void ()>& function)
+void ThreadPool::add_task(const std::function<void ()> function)
 {
     if (state == State::STOPPED)
         return;
 
-    std::unique_lock<std::recursive_mutex> lock(*mutex_);
+    std::unique_lock<std::recursive_mutex> lock(mutex_);
     tasks_.push(function);
-    cv_->notify_one();
+    cv_.notify_one();
 }
 
 void ThreadPool::remove_all_tasks()
 {
-    std::unique_lock<std::recursive_mutex> lock(*mutex_);
-    while (!tasks_.size())
+    std::unique_lock<std::recursive_mutex> lock(mutex_);
+    while (tasks_.size())
         tasks_.pop();
 }
 
@@ -45,13 +47,32 @@ void ThreadPool::destroy()
     if (state == State::STOPPED)
         return;
 
-    std::unique_lock<std::recursive_mutex> lock(*mutex_);
-    if (!tasks_.size())
-        this->remove_all_tasks();
-    lock.unlock();
+    this->remove_all_tasks();
 
     for (std::shared_ptr<Executor> executor : threads_)
         executor->stop();
     threads_.clear();
     state = State::STOPPED;
+    cv_.notify_all();
+}
+
+void ThreadPool::sync()
+{
+    std::unique_lock<std::recursive_mutex> lock(mutex_);
+    while (tasks_.size())
+    {
+        lock.unlock();
+        std::this_thread::yield();
+        lock.lock();
+    }
+
+    for (auto thread : threads_)
+    {
+        while (!thread->is_waiting())
+        {
+            lock.unlock();
+            std::this_thread::yield();
+            lock.lock();
+        }
+    }
 }
