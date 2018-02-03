81c9edea11a6fc64f6ad5e91dcc71fd3171b4297
Dominique <dominique.besson@epita.fr>
2018-02-03 01:13:42 +0100

    Executor: started implementing contructor

--- src/threadpool/executor.cc
+++ src/threadpool/executor.cc
@@ -1,5 +1,12 @@
 #include "executor.hh"
 
+Executor::Executor(const std::shared_ptr<std::recursive_mutex>& mutex,
+                   const std::shared_ptr<std::condition_variable>& cv)
+    : cv_(cv), mutex_(mutex)
+{
+    std::make_shared<std::thread>(start);
+}
+
 void Executor::start()
 {
 
