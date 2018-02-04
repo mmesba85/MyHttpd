a996a4af0e85cb232c62a8a05df916974ee91495
Melissa Li <melissali999@hotmail.com>
2018-02-04 01:31:34 +0100

    Implemented setter and getter in the ServerConfig class

--- src/server_config.hxx
+++ src/server_config.hxx
@@ -29,3 +29,33 @@ std::string ServerConfig::get_ip() const
 {
 	return ip_;
 }
+
+// void ServerConfig::insert_to_configuration(std::string& a, std::string& b)
+// {
+//   configurations_.insert(std::pair<std::string, std::string>(a, b));
+// }
+
+void ServerConfig::set_server_name(std::string& server_name)
+{
+  server_name_ = server_name;
+}
+
+void ServerConfig::set_port(std::string& port)
+{
+  port_ = port;
+}
+
+void ServerConfig::set_ip(std::string& ip)
+{
+  ip_ = ip;
+}
+
+void ServerConfig::set_root_dir(std::string& root_dir)
+{
+  root_dir_ = root_dir;
+}
+
+// void insert_to_error(std::string& a, std::string& b);
+// {
+//   error_.insert(std::pair<std::string, std::string>(a, b));
+// }
