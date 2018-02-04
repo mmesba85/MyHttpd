2434207c1fa0523161ed4412d7dd12db4cc55a4c
Melissa Li <melissali999@hotmail.com>
2018-02-04 01:30:53 +0100

    Added setter and getter header in the ServerConfig class

--- src/server_config.hh
+++ src/server_config.hh
@@ -18,6 +18,8 @@ private:
   std::string ip_;
   std::string root_dir_;
 
+  std::map<std::string, std::string> error_;
+
 public:
   ServerConfig();
   ServerConfig(std::string name, std::string ip, std::string port,
@@ -25,13 +27,13 @@ public:
   virtual ~ServerConfig();
   std::string get_port() const;
   std::string get_ip() const;
-  // std::map<std::string, std::string>& get_configurations();
-  // void set_configurations(std::map<std::string, std::string>& configuration);
-  // std::string& get_server_name();
-  // std::string& get_port();
-  // std::string& get_ip();
-  // std::string& get_root_dir();
 
+  void insert_to_configuration(std::string& a, std::string& b);
+  void set_server_name(std::string& server_name);
+  void set_port(std::strin&g port);
+  void set_ip(std::string& ip);
+  void set_root_dir(std::string& root_dir);
+  void insert_to_error(std::string& a, std::string& b);
 };
 
 #include "server_config.hxx"
