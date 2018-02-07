b0b6f025f92560e741f57ec217efa1ad9c25dc39
Maroua MESBAHI <maroua.mesbahi@epita.fr>
2018-02-07 23:37:50 +0100

    Removed check_request method

--- src/main.cc
+++ src/main.cc
@@ -3,97 +3,11 @@
 #include <vector>
 #include <iostream>
 #include "server_connection.hh"
+#include "request.hh"
 
 constexpr int max_listen = 1000;
 constexpr int max_events = 1000;
 constexpr int max_request_len = 255;
-
-
-/* check the request line 
-  (the first line of the request)
-  */
-
-bool check_request_line(std::string line)
-{
-  std::vector<std::string> strings;
-  std::istringstream g(line);
-  std::string s;
-  while (getline(g, s, ' '))
-    strings.push_back(s);
-
-  if(strings.size() != 3)
-    return false;
-  if(strings[0].compare("GET") != 0 && 
-    strings[0].compare("POST") != 0)
-    return false;
-  
-  if(strings[2].compare("HTTP/1.1") != 0 &&
-     strings[2].compare("HTTP/1.0") != 0)
-    return false;  
-  return true;
-}
-
-/* check the request */
-bool check_request(std::string request)
-{
-  std::istringstream f(request);
-  std::string s;    
-  getline(f, s);
-
-  std::cout << "first line : " << s << std::endl;
-  s.erase(s.length()-1);
-
-  if (!check_request_line(s))
-    return false;
-
-  std::cout << "ici" << std::endl;
-  /* marque l'arrivee a la fin de la requete
-      si une autre ligne est lu apres cette fin
-      alors erreur
-  */
-  auto is_end = 0;
-  while(getline(f, s))
-  {
-    std::vector<std::string> strings;
-    std::istringstream g(s);
-    std::string aux;
-    
-
-    if(is_end == 1)
-      return false;
-
-    /* si le dernier caractere de la ligne est different
-      de \r alors erreur
-    */
-    size_t len = s.length();
-    if(s.at(len-1) != '\r')
-      return false;
-
-    /* on cherche la fin de requete */
-    if(s.compare("\r") == 0)
-    {
-      is_end = 1;
-      continue;
-    }
-
-    /* on decoupe la ligne en champs */
-    while(getline(g, aux, ' '))
-      strings.push_back(s);
-    
-    /* s'il y a plus de deux mots alors erreur */
-    if(strings.size() != 2)
-      return false;
-    
-    /* si l'argument du head connection est different 
-      de close ou de keep-alive alors erreur
-    */
-    if(strings[0].compare("Connection:") == 0 && 
-        strings[1].compare("close") != 0 &&
-        strings[1].compare("keep-alive") != 0)
-      return false;
-  }
-  return true;
-}
 
 /**
 ** call the appropriate method
@@ -102,6 +16,7 @@ bool check_request(std::string request)
 void process_request(std::string request, int fd)
 {
   bool is_valid = check_request(request);
+  
   std::cout << "is valid : " << is_valid << std::endl;
   std::cout << fd;
 }
