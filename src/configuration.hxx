1cbe843d7f29616137c3c759ef98de7e0ddf3b9f
Melissa Li <melissali999@hotmail.com>
2018-02-07 09:42:31 +0100

    Removed some debug print in the fill_configuration function

--- src/configuration.hxx
+++ src/configuration.hxx
@@ -142,18 +142,15 @@ void Configuration::fill_configuration()
 
     try
     {
-      if (it->find("error") != it->end())
-        std::cout << "TOTO" << std::endl;
-      auto error = toml::get<toml::Array<toml::Array<toml::String>>>(it->at("error"));
-      std::cout << "COUCOU" << std::endl;
-      std::cout << "COUCOU" << std::endl;
+      // if (it->find("error") != it->end())
+      //   std::cout << "TOTO" << std::endl;
+      auto error =
+          toml::get<toml::Array<toml::Array<toml::String>>>(it->at("error"));
       for (auto iu = error.begin(); iu != error.end(); ++iu)
         one.insert_to_error(iu->at(0), iu->at(1));
     }
     catch(const std::exception& e)
-    {
-      std::cout << e.what() << std::endl;
-    }
+    {}
 
     list_server_.push_back(one);
   }
