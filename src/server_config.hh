bb7e8036280c2d7f4b47e838dd8763a4f918a8c5
Melissa Li <melissali999@hotmail.com>
2018-02-07 09:41:21 +0100

    Some documentation in server_config.hh

--- src/server_config.hh
+++ src/server_config.hh
@@ -9,29 +9,31 @@
 class ServerConfig
 {
 private:
-  std::map<std::string, std::string> configurations_;
-
   std::string server_name_;
   std::string port_;
   std::string ip_;
   std::string root_dir_;
 
+  std::map<std::string, std::string> configurations_;
   std::map<std::string, std::string> error_;
 
 public:
+  /* Constructors */
   ServerConfig();
   ServerConfig(std::string name, std::string ip, std::string port,
     std::string root_dir);
   virtual ~ServerConfig();
-  std::string get_port() const;
-  std::string get_ip() const;
 
-  void insert_to_configuration(std::string a, std::string& b);
+  /* Getter and setters */
+   std::string get_port() const;
+   std::string get_ip() const;
   void set_server_name(std::string& server_name);
   void set_port(std::string& port);
   void set_ip(std::string& ip);
   void set_root_dir(std::string& root_dir);
   void insert_to_error(std::string& a, std::string& b);
+
+  /* Print this server configuration */
   void print();
 };
 
