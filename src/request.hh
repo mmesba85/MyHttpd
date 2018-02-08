ccab939c887a5fdde306f83b281e70efab74f4d2
Maroua MESBAHI <maroua.mesbahi@epita.fr>
2018-02-08 20:30:34 +0100

    Updated interface with member functions

--- src/request.hh
+++ src/request.hh
@@ -1,18 +1,22 @@
 #pragma once
 
+#include "response.hh"
+#include "server_config.hh"
+
 class Request
 {
   public:
-    Request(std::string url, int version, std::string host, 
-      bool connected);
-    bool check_request() const;
-    bool check_version() const;
-    bool check_url() const;
-    static bool check_request_line(std::string line);
+    bool bad_method() const;
+    bool forbidden(ServerConfig& config) const;
+    bool not_found(ServerConfig& config) const;
+    std::string& get_version();
+    static bool check_request_line(std::string& line);
     static bool check_client_request(std::string request);
+    static std::string get_method(std::string& request);
+    //virtual std::string process_request(Response& rp, ServerConfig& config) const = 0;
   protected:
     std::string url_;
-    int version_;
+    std::string version_;
     std::string host_;
     bool connected_;
 };
