b6a2ec99eee98308de35161dd3c854e6725f7d7a
Maroua Mesbahi <maroua.mesbahi@epita.fr>
2018-02-14 22:43:17 +0100

    Updated constructor for compilation purpose

--- src/cgi-handler.cc
+++ src/cgi-handler.cc
@@ -1,3 +1,5 @@
 #include "cgi-handler.hh"
 
-
+CgiHandler::CgiHandler(std::string env_path)
+: env_path_(env_path)
+{}
--- src/cgi-handler.hh
+++ src/cgi-handler.hh
@@ -2,7 +2,7 @@
 
 #include <string>
 
-#include "request.hh"
+class Request;
 
 class ServerConfig;
 
@@ -10,8 +10,8 @@ class CgiHandler
 {
     public:
         CgiHandler(std::string env_path);
-        int process(Request& req_obj, ServerConfig& conf, std::string& rep);
-        void cancel();
+        //int process(Request& req_obj, ServerConfig& conf, std::string& rep);
+        //void cancel();
     private:
         std::string env_path;
 };
