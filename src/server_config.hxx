1286fcf3312c85053acc9ebf8ef8d2578aa597c8
Dominique <dominique.besson@epita.fr>
2018-02-14 21:37:39 +0100

    CGI: added cgi initialisation

--- src/server_config.hxx
+++ src/server_config.hxx
@@ -1,17 +1,18 @@
 #include "server_config.hh"
 
 ServerConfig::ServerConfig()
+    : cgi_handler_("")
 {
 }
 
 ServerConfig::ServerConfig(std::string name, std::string port, std::string ip,
   std::string root_dir)
+    : cgi_handler_("")
 {
   server_name_ = name;
   port_ = port;
   ip_ = ip;
   root_dir_ = root_dir;
-
 }
 
 ServerConfig::~ServerConfig()
