f7b03a35b7921d9dd10a3c1afc75d7b263658b38
Maroua Mesbahi <maroua.mesbahi@epita.fr>
2018-02-16 14:43:26 +0100

    updated constructor

--- src/server_connection.hh
+++ src/server_connection.hh
@@ -6,15 +6,18 @@
 class ServerConnection
 {
   public:
-    ServerConnection();
+    ServerConnection()
+    {}
+    ServerConnection(ServerConfig& server);
     ~ServerConnection();
-    ThreadPool& get_pool();
+   // ThreadPool* get_pool();
     ServerConfig& get_config();
     int get_socket() const;
     bool set_connection(struct epoll_event& event, int epoll_fd);
+    void set_socket(int& socket);
   private:
     ServerConfig server_;
-    ThreadPool th_pool_;
+  //  ThreadPool th_pool_;
     int socket_;
 };
 
--- src/server_connection.hxx
+++ src/server_connection.hxx
@@ -12,52 +12,15 @@
 #include <string>
 #include "server_connection.hh"
 
-ServerConnection::ServerConnection()
-{
-  server_ = ServerConfig("tests", "4242", "127.0.0.1", "./tests", "");
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
+ServerConnection::ServerConnection(ServerConfig& server)
+: server_(server)
+{}
 
-  if(sock < 0)
-  {
-    std::error_code ec(errno, std::generic_category());
-    throw std::system_error(ec, "error socket creation.");  
-  }
-
-  struct sockaddr_in sin;
-  sin.sin_family = AF_INET;
-  std::string::size_type sz;
-  int port = std::stoi(server_.get_port(), &sz);
-  sin.sin_port = htons(port);
-  const char* ip = server_.get_ip().c_str();
-  sin.sin_addr.s_addr = inet_addr(ip);
-  
-  int reuse = 1;
-  if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (
-    const char*)&reuse, sizeof(reuse)) < 0)
-  {
-    std::error_code ec(errno, std::generic_category());
-    throw std::system_error(ec, "setsockopt(SO_REUSEPORT) failed.");
-  }
-
-  if(bind(sock, (struct sockaddr *)&sin, sizeof sin) == -1)
-  {
-    std::error_code ec(errno, std::generic_category());
-    throw std::system_error(ec, "bind failed.");
-  }
-
-  if(fcntl(sock, F_SETFL, O_NONBLOCK) < 0)
-  {
-    std::error_code ec(errno, std::generic_category());
-    throw std::system_error(ec, "fcntl failed.");
-  }  
-
-  socket_ = sock;
-}
 
 ServerConnection::~ServerConnection()
 {
   close(socket_);
-  th_pool_.destroy();
+ // th_pool_->destroy();
 }
 
 bool ServerConnection::set_connection(struct epoll_event& event, 
@@ -65,7 +28,6 @@ bool ServerConnection::set_connection(struct epoll_event& event,
 {
   struct sockaddr in_addr;
   socklen_t len = sizeof(in_addr);
-  std::cout << "** " << socket_ << std::endl;
   int fd = accept(socket_, &in_addr, &len);
 
   if(fd == -1)
@@ -108,12 +70,17 @@ int ServerConnection::get_socket() const
   return socket_;
 }
 
-ThreadPool& ServerConnection::get_pool()
+/*ThreadPool* ServerConnection::get_pool()
 {
   return th_pool_;
-}
+}*/
 
 ServerConfig& ServerConnection::get_config()
 {
   return server_;
+}
+
+void ServerConnection::set_socket(int& socket)
+{
+  socket_ = socket;
 }
